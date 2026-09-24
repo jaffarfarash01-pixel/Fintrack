1e75099e58d539c24c651d02e465e93369c354c2
jaffar <jaffarfarash01@gmail.com>
2026-09-24 09:57:08 +0530

    Prepare backend for deployment

--- backend/server.js
+++ backend/server.js
@@ -14,12 +14,7 @@ const app = express();
 
 // const cors = require("cors");
 
-app.use(cors({
-  origin: [
-    "http://localhost:5500",
-    "http://127.0.0.1:5500"
-  ]
-}));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
